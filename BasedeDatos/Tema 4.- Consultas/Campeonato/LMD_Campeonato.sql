c66331bf5582cefab8e9ce9b53aa3b931d161e97
ヴィモ <personalusevictor@gmail.com>
2026-02-25 14:18:53 +0000

    SQL - LMD Campeonato

--- BasedeDatos/Tema 4.- Consultas/Campeonato/LMD_Campeonato.sql
+++ BasedeDatos/Tema 4.- Consultas/Campeonato/LMD_Campeonato.sql
@@ -39,3 +39,24 @@ SELECT * FROM juego j INNER JOIN equipo e WHERE megusta IS NULL AND j.cdequipo =
 
 /* 5.- Listado con el nombre, dificultad, megusta y código de equipo de los juegos  organizados por el mismo equipo que el del juego de nombre 'Elvenar'. (Con subconsulta) */
 SELECT nombre, dificultad, megusta, cdequipo AS 'código de equipo' FROM juego WHERE cdjuego IN (SELECT cdjuego FROM juego WHERE nombre like 'Elvenar');
+
+/*6.- Modifica la consulta 5. para que también se muestre el nombre y comunidad del equipo.*/
+SELECT j.nombre, j.dificultad, j.megusta, j.cdequipo  AS 'código de equipo', e.nombre, e.comunidad FROM juego j INNER JOIN equipo e ON j.cdequipo like e.cdequipo WHERE j.cdjuego IN (SELECT cdjuego FROM juego WHERE nombre like 'Elvenar');
+
+/* 7.- Para los concursantes que participan en juegos, listar el código y nombre de concursantes y código del juego en el que participan, mostrando los puntos y fecha de inicio. Ordena el listado alfabéticamente por código de concursante.*/
+SELECT c.cdconcur, c.nombre, p.cdjuego, p.puntos, p.fecha_inicio FROM concursante c JOIN participa p ON c.cdconcur like p.cdconcur ORDER BY c.cdconcur ASC;
+
+/* 8.- Lista donde aparezcan los códigos y nombre de todos los concursantes, nombre de sus equipos y nombre de los juegos en los que participan, así como los puntos que llevan en cada juego. Los concursantes que no participan en juegos aparecerán también en la lista y en el lugar del juego aparecerá “sin juego” y en horas un cero. Debes poner un encabezado apropiado de manera que no aparezcan nulos en el listado.*/
+SELECT c.cdconcur, c.nombre, ifnull(e.nombre, "sin equipo") 'equipo', ifnull(j.nombre, "sin juego") 'juego', ifnull(p.puntos, 0) 'puntos', ifnull(p.fecha_inicio, "sin fecha") 'fecha' FROM concursante c LEFT JOIN equipo e ON c.cdequipo like e.cdequipo LEFT JOIN participa p ON c.cdconcur like p.cdconcur LEFT JOIN juego j ON p.cdjuego like j.cdjuego;
+
+/* 9.- Para cada concursante que participa en juegos, su código, nombre y la media de sus puntos. Redondear la media de puntos con dos decimales, (usa la función ROUND()) y ordenar de más a menos puntos.*/
+SELECT c.cdconcur, c.nombre, ROUND(AVG(p.puntos), 2) AS media_puntos FROM concursante c INNER JOIN participa p ON c.cdconcur = p.cdconcur GROUP BY c.cdconcur, c.nombre ORDER BY media_puntos DESC;
+
+/* 10.- Modifica la consulta 9. para que aparezca el código y nombre del ídolo de cada concursante. Limita el listado a los 5 concursantes con la media más alta.*/
+SELECT c.cdconcur, c.nombre, ROUND(AVG(p.puntos), 2) AS media_puntos, i.cdconcur, i.nombre FROM concursante c INNER JOIN participa p ON c.cdconcur = p.cdconcur INNER JOIN concursante i ON c.cdidolo like i.cdconcur GROUP BY c.cdconcur, c.nombre, i.cdconcur, i.nombre ORDER BY media_puntos DESC;
+
+/* 11.- Listado de los nombres de juegos en los que aparezca la palabra “Empire”, indicando los megusta que tiene, dificultad y  total de concursantes que participan en el juego. */
+SELECT j.nombre, IFNULL(j.megusta, 0) AS "Me gusta", j.dificultad, COUNT(p.cdconcur) AS "Nº Concursantes" FROM juego j INNER JOIN participa p ON j.cdjuego like p.cdjuego WHERE j.nombre LIKE '%Empire%' GROUP BY j.cdjuego, j.nombre, j.megusta, j.dificultad;
+
+/* 12.- Listado con el código de juego y total de concursantes que participan en cada juego. En el listado mostrar solo los juegos en los que participan 2 o más concursantes. Ordenar de más  menos paticipación.*/
+SELECT p.cdjuego, COUNT(p.cdconcur) AS "cuenta" FROM participa p GROUP BY p.cdjuego HAVING COUNT(cdconcur) >= 2 ORDER BY p.cdjuego;
